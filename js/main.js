9dcfff3d57c8a1f079f61d5e79dfca83a774159f
BFB_Server_wwwuser <hal9000@blackforestbytes.de>
2018-05-31 01:00:05 +0200

    target=blank for open button

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function refreshEntries()
 			tr    += "    <a class=\"btn_action btn_disabled\" onclick=\"if($(this).hasClass('btn_disabled'))return false; doPull("+i+", false);return false;\" href=\"#\">Pull</a>";
 			tr    += "    <a class=\"btn_action btn_disabled\" onclick=\"if($(this).hasClass('btn_disabled'))return false; doPull("+i+", true);return false;\" href=\"#\">Force Pull</a>";
 			tr    += "    <a class=\"btn_action btn_disabled\" onclick=\"if($(this).hasClass('btn_disabled'))return false; doRefresh("+i+");return false;\" href=\"#\">Refresh</a>";
-			tr    += "    <a class=\"btn_action btn_disabled btn_action_url\" onclick=\"if($(this).hasClass('btn_disabled'))return false; return true;\" href=\"#\">Open</a>";
+			tr    += "    <a class=\"btn_action btn_disabled btn_action_url\" onclick=\"if($(this).hasClass('btn_disabled'))return false; return true;\" target=\"_blank\" href=\"#\">Open</a>";
 			tr    += "  </td>";
 			tr    += "</tr>"
 			table_body.append(tr);
